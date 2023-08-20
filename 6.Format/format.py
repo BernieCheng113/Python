73cf24b259d6557dc540aa534e1d6fc65507d420
Bernie <BernieCheng113@gmail.com>
2023-08-21 00:07:02 +0800

    'format'

---
+++ 6.Format/format.py
@@ -0,0 +1,15 @@
+#print format-1
+city='Amsterdam'
+country ='Netherlands'
+print('format-1→City',city,'is in the country',country)
+
+#print format-2
+print('format-2→City {} is in the country {}'.format(city,country))
+
+
+#print format-3
+print('format-3→City {0} is in the country {1},yes in{0}'.format(city,country))
+
+
+#print format-4
+print('format-4→City %s is in the country %s' % (city,country))
