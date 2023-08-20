8c19ace32c6ca7b22a1d681dd3fe95de245104ed
Bernie <BernieCheng113@gmail.com>
2023-08-21 00:14:28 +0800

    'CheckType'

--- 3.type/CheckType.py
+++ 3.type/CheckType.py
@@ -1,3 +1,4 @@
+#Base View Type
 x = 'PythonGood'
 print(type(x))
 
@@ -7,3 +8,14 @@ print(type(y))
 z=3.5
 type_name = type(z).__name__
 print(type_name)
+
+#Type Operations int
+print('輸入兩個數字')
+int1 = eval(input('請輸當分子的數'))
+int2 = eval(input('請輸當分母的數'))
+
+Result = int1/int2
+type_name = type(Result).__name__
+print('Get Type:' + type_name +'  ' + 'Val:' + str(Result))
+
+
