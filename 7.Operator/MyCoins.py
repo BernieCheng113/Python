579a9733f0992992c95afa28878c2f8c3b61c3b6
Bernie <BernieCheng113@gmail.com>
2023-08-21 21:54:42 +0800

    '20230821_add_by_Bernie_for_Add_Operator'

---
+++ 7.Operator/MyCoins.py
@@ -0,0 +1,21 @@
+MyMoney = 9999
+
+MyMoney = MyMoney -2000
+
+ATM = 3000
+
+MyMoney = MyMoney + ATM
+
+Food=75
+
+MyMoney = MyMoney - round(Food* 0.8)
+
+MyATM = MyMoney 
+
+#列出剩餘金額
+print('Save MyCoins' + str(MyATM)) 
+
+
+
+
+
